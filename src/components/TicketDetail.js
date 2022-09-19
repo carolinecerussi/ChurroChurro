f485dec0c66315e25dff27fee1007537a4d05a15
Kyle Chinick <kylechinick@icloud.com>
2022-09-19 16:25:07 -0700

    Add firebase and swapped to firebase ID generator over UUID, tested ticket submission to db
    Co-authored-by: jacob palaoro <jpalaoro197@gmail.com>
    Co-authored-by: Caroline Cerussi <caroceru@gmail.com>

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+
 function TicketDetail(props){
   const { ticket, onClickingDelete, onClickingEdit } = props; 
 
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,6 +2,7 @@ import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
 
+
 function TicketList(props){
 
   return (
@@ -13,7 +14,6 @@ function TicketList(props){
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          id={ticket.id}
           key={ticket.id}/>
       )}
     </React.Fragment>
