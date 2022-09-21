7ab1bf48107c4a7473cf54494f9e5f121f126182
Kyle Chinick <kylechinick@icloud.com>
2022-09-21 15:17:14 -0700

    Add support for styled-components library, refactor Header component to use library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import { Link } from "react-router-dom";
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+  background-color: purple;
+`;
 
-function Header(){
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
+
+function Header() {
   return (
-    <React.Fragment>
-      <h1> Help Queue</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/sign-in">Sign In</Link>
-        </li>
-      </ul>
-    </React.Fragment>
+    <StyledWrapper>
+      <React.Fragment>
+        <HelpQueueHeader>
+          <h1>Help Queue</h1>
+        </HelpQueueHeader>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/sign-in'>Sign In</Link>
+          </li>
+        </ul>
+      </React.Fragment>
+    </StyledWrapper>
   );
 }
-
 
 export default Header;
